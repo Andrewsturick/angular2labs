efddff90be6e29bbdba932effb06f10cb7851971
Andrew Sturick <andrew.sturick@gmail.com>
2016-07-12 10:13:22 -0700

    lab 1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import {HTTP_PROVIDERS, Http} from '@angular/http';
+import 'rxjs/Rx'; // For methods for Observables
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  providers: [HTTP_PROVIDERS],
+  selector: 'my-app',
+  templateUrl: './app/app.component.html',
 })
-export class AppComponent { }
+export class AppComponent {
+  server = 'http://localhost:8080';
+  people = [];
+
+  constructor(private http: Http) {
+  }
+
+  checkSearch(term) {
+    if (term.length < 2) {
+      this.people = [];
+    } else {
+      this.http.get(this.server + '/people/' + term)
+        .map((res) => res.json())
+        .subscribe((response) => {
+          this.people = response.people;
+        });
+    }
+  }
+}
